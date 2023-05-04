3b00cd6557f44bb64476d5dd702750b192b458f6
faiyaz032 <faiyazrahman03@gmail.com>
2023-05-05 01:30:48 +0600

    extract url from query

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-app.use(cors({ origin: true }));
+app.use(
+  cors({
+    origin: [
+      'http://127.0.0.1:5501',
+      'https://6453fcca78622b3089052779--celebrated-paprenjak-b024f4.netlify.app',
+    ],
+  })
+);
 
 app.use(bodyParser.json());
 
@@ -14,16 +21,22 @@ app.get('/', (req, res) => {
 
 app.get('/screenshot', async (req, res) => {
   console.log(req.query);
-  let { x, y, width, height, clientWidth, clientHeight, Yoffset } = req.query;
+  let { x, y, width, height, clientWidth, clientHeight, Yoffset, url } = req.query;
   if (isNaN(x) || isNaN(y) || isNaN(width) || isNaN(height)) {
     res.status(400).send('Invalid parameters');
     return;
   }
 
+  // Validate URL parameter
+  if (!url) {
+    res.status(400).send('Missing URL parameter');
+    return res.json('Url parameter is missing');
+  }
+
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  await page.goto(req.headers.referer);
+  await page.goto(url);
   await page.setViewport({ width: +clientWidth, height: +clientHeight });
   //   await page.evaluate(`window.scrollBy(0, 940)`); // scroll to a height of 1000 pixels
   if (+y < +Yoffset) {
