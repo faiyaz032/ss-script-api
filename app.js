e2f387a01960b4f9787886607e601d22ee2f139a
faiyaz032 <faiyazrahman03@gmail.com>
2023-05-05 16:38:28 +0600

    rolledBack

--- app.js
+++ app.js
@@ -2,54 +2,29 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 const app = express();
 const cors = require('cors');
-const bodyParser = require('body-parser');
-require('dotenv').config();
+const bodyPerser = require('body-parser');
 
-app.use(
-  cors({
-    origin: true,
-  })
-);
-
-app.use(bodyParser.json());
+app.use(cors({ origin: true }));
+app.use(bodyPerser.json());
 
 app.get('/', (req, res) => {
-  res.json('Server is Live');
+  res.send('Changed to Docker');
 });
 
 app.get('/screenshot', async (req, res) => {
   console.log(req.query);
-  let { x, y, width, height, clientWidth, clientHeight, Yoffset, url } = req.query;
+  let { x, y, width, height, clientWidth, clientHeight, Yoffset } = req.query;
   if (isNaN(x) || isNaN(y) || isNaN(width) || isNaN(height)) {
     res.status(400).send('Invalid parameters');
     return;
   }
 
-  // Validate URL parameter
-  if (!url) {
-    res.status(400).send('Missing URL parameter');
-    return res.json('Url parameter is missing');
-  }
-
   const browser = await puppeteer.launch({
-    executablePath:
-      process.env.NODE_ENV === 'production'
-        ? process.env.PUPPETEER_EXECUTABLE_PATH
-        : puppeteer.executablePath(),
-    args: [
-      '--disable-setuid-sandbox',
-      '--no-sandbox',
-      '--single-process',
-      '--no-zygote',
-      '--disable-web-security',
-      '--disable-features=IsolateOrigins',
-      '--disable-site-isolation-trials',
-      '--disable-features=BlockInsecurePrivateNetworkRequests',
-    ],
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--single-process', '--no-zygote'],
   });
   const page = await browser.newPage();
 
-  await page.goto(url);
+  await page.goto(req.headers.referer);
   await page.setViewport({ width: +clientWidth, height: +clientHeight });
   //   await page.evaluate(`window.scrollBy(0, 940)`); // scroll to a height of 1000 pixels
   if (+y < +Yoffset) {
@@ -59,6 +34,8 @@ app.get('/screenshot', async (req, res) => {
     clip: { x: +x, y: +y, width: +width, height: +height },
   });
 
+  console.log('🚀 ~ file: app.js:32 ~ app.get ~ screenshotBuffer:', screenshotBuffer);
+
   res.set('Content-Type', 'image/png');
   res.send(screenshotBuffer);
   // console.log(y, width, height);
@@ -67,7 +44,6 @@ app.get('/screenshot', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
-  console.log(`Server is alive on PORT:${PORT}`);
+  console.log('Server running on port 3000');
 });
